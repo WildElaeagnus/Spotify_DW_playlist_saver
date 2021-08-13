660e60b8fa88f5bbfb302e94ef0d657bb8eb1f7b
WildElaeagnus <akorzunin123@gmail.com>
2021-08-13 19:45:57 +0300

    0.3

--- Spotify_DW_playlist_saver.py
+++ Spotify_DW_playlist_saver.py
@@ -2,6 +2,8 @@
 import requests
 import SpotifyAPI
 import configparser
+import json
+
 from urllib.parse import urlencode
 
 # read file with auth info if it not exist then create it
@@ -23,24 +25,54 @@ print(spotify_login.perform_auth())
 token = spotify_login.access_token
 print(token)
 
-# how to use token
-# user_id = 'wizzler'
-pl_endpoint = f'https://api.spotify.com/v1/users/{user_id}/playlists'
-sh_endpoint = f'https://api.spotify.com/v1/search'
-header = {
+def url(pl_endpoint, pl_data):
+    '''form a valid url from endpoint and data'''
+    return f'{pl_endpoint}?{pl_data}'
+
+# header for request
+headers = {
     "Authorization": f"Bearer {token}"
 }
-data = urlencode({"q": "time", "type": "track"})
-pl_data = urlencode({"limit": 5})
-q_url = f'{pl_endpoint}?{pl_data}'
-r = requests.get(q_url, headers=header)
-print(r.json())
+# request to get DW playlist
+pl_endpoint = f'https://api.spotify.com/v1/users/{user_id}/playlists'
+pl_data = urlencode({"limit": 50})
+r = requests.get(url(pl_endpoint, pl_data), headers=headers)
+# find DW playlist id in response
+loaded_json = json.loads(r.text)
+items = loaded_json['items']
+for i in items:
+	if i['name'] == 'Discover Weekly':
+		DW_playlist_url = i['external_urls']
+		DW_playlist_href = i['href']
+        # TODO add a condition if DW playlist not found
+DW_id = DW_playlist_href.split("/")[-1]
+print(f'DW_id: {DW_id}')
+
+# create dict with tracks ids and names from req
+pl_endpoint = f'https://api.spotify.com/v1/playlists/{DW_id}/tracks'
+pl_data = urlencode({"market": "Ru"})
+r = requests.get(url(pl_endpoint, pl_data), headers=headers)
+loaded_json = json.loads(r.text)
+items = loaded_json['items']
+tracks_dw = {}
+# id is a key cuz names can be same
+for i in range(loaded_json['total']):
+    j = items[i]['track']
+    tracks_dw[j['id']] = j['name']
+print(tracks_dw)
+
+# play first song from dict and see req code
+
+# anyway 
+# send req to play song
+# send req what playing right now
+# if response dont math then pass
+# if math add key and val to new dict dw_likes
+# after loaded_json['total'] cycles 
+# create a req to make new playlist with dw_likes dict
+
+# after all figure out how to scedule this thing
 # TODO 
-# GET	/v1/playlists/{playlist_id}/tracks	Get a Playlist's Items
-# implement code from notebook
-# parse out DW playlist id
-# mb detect user id automatically
-# 
 # u can find out if song was disliked if it not appears when
 # this py script trying to play it on any devise
 # (but what to do if none of devices is up???)
